9a5fbe15515b4bc5d1e1af3c1b8b2dc22b5fe749
Adair <Adair520@126.com>
2018-09-06 22:29:47 +0800

    2018-9-6再次更新

--- 13-UIWebview/Web13ViewController.swift
+++ 13-UIWebview/Web13ViewController.swift
@@ -8,28 +8,35 @@
 
 import UIKit
 
-class Web13ViewController: UIViewController {
+class Web13ViewController: UIViewController,UIWebViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let bounds = UIScreen.main.bounds
+        let frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+        let webView = UIWebView(frame: frame)
+        webView.delegate = self
+        webView.backgroundColor = UIColor.clear
+        
+        let url = URL(string: "https://www.apple.com/")
+        let urlRequest = NSURLRequest(url: url!)
+        webView.loadRequest(urlRequest as URLRequest)
+        
+        self.view.addSubview(webView)
     }
-
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
