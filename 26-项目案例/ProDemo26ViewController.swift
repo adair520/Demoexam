f85a42ba13b6536512100b581fc07f7deb713315
Adair <Adair520@126.com>
2019-05-21 22:54:03 +0800

    Update ProDemo26ViewController.swift
    更新了很小的一部分内容2019-5-21

--- 26-项目案例/ProDemo26ViewController.swift
+++ 26-项目案例/ProDemo26ViewController.swift
@@ -7,29 +7,10 @@
 //
 
 import UIKit
-
+/*这里主要是集成一些小项目供参考学习使用*/
 class ProDemo26ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
